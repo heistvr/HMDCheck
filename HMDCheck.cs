0fbddd3e1552a0010c2bf2186182fb77d02f7a79
NatoshiSakomoto <108589206+NatoshiSakomoto@users.noreply.github.com>
2022-08-02 20:43:42 -0700

    Add files via upload

---
+++ HMDCheck.cs
@@ -0,0 +1,109 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class HMDCheck : UdonSharpBehaviour
+{
+    private VRCPlayerApi _player = Networking.LocalPlayer;
+    
+    //Set object references in scene Inspector window to drag and drop teleport locations for users
+    public Transform tp_vr;
+    public Transform tp_desktop;
+
+    //Allow list of usernames which can bypass the HMDCheck
+    public string[] _adminUsers;
+
+    //Bypass check for HMDToggle
+    public bool bypass = false;
+
+    void Start() {
+        //Only apply to local player -- not global or synced
+        _player = Networking.LocalPlayer;
+    }
+
+
+    private bool CheckAllowListStatus()
+    {
+        bool allowlist = false;
+        foreach (string _adminPlayers in _adminUsers)
+        {
+            if (Networking.LocalPlayer.displayName == _adminPlayers)
+                allowlist = true;
+        }
+
+        return allowlist;
+    }
+
+//HMDCheck when player spawns into the collider field that this script is attached to
+//Comment out this section and use the below "Interact()" section to use an interact object instead
+   /* 
+    public override void OnPlayerTriggerStay(VRCPlayerApi _player)
+    {
+        bool allowlist = CheckAllowListStatus();
+        //Check if player is in VR or allow list bypass and then teleport to the corresponding location
+        if (_player.IsUserInVR() || allowlist)
+        {
+            _player.TeleportTo(tp_vr.transform.position, tp_vr.transform.localRotation);
+        }
+        else
+        {
+            _player.TeleportTo(tp_desktop.transform.position, tp_desktop.transform.localRotation);
+            _player.Immobilize(true);
+        }
+    }
+    */
+
+//HMDCheck when player interacts with an object that this script is attached to
+//Comment out this section and use the above "OnPlayerTriggerStay()" section to use a collider field instead
+
+public override void Interact()
+    {
+        if (!bypass)
+        {
+            bool allowlist = CheckAllowListStatus();
+
+            //Check if local player an admin or staff, and if local player is in VR and teleport to vrYes if so, vrNo if not
+            if (_player.IsUserInVR() || allowlist)
+            {
+                _player.TeleportTo(tp_vr.transform.position, tp_vr.transform.localRotation);
+            }
+            else
+            {
+                _player.TeleportTo(tp_desktop.transform.position, tp_desktop.transform.localRotation);
+                _player.Immobilize(true);
+            }
+        }
+        else
+        {
+            _player.TeleportTo(tp_vr.transform.position, tp_vr.transform.localRotation);
+            _player.Immobilize(false);
+        }
+    }
+
+    //HMDCheck players on respawn.
+
+public override void OnPlayerRespawn(VRCPlayerApi _player)
+    {
+        if (!bypass)
+        {
+            bool allowlist = CheckAllowListStatus();
+            //Check if local player an admin or staff, and if local player is in VR and teleport to vrYes if so, vrNo if not
+            if (_player.IsUserInVR() || allowlist)
+            {
+                _player.TeleportTo(tp_vr.transform.position, tp_vr.transform.localRotation);
+            }
+            else
+            {
+                _player.TeleportTo(tp_desktop.transform.position, tp_desktop.transform.localRotation);
+                _player.Immobilize(true);
+            }
+        }
+        else
+        {
+            _player.TeleportTo(tp_vr.transform.position, tp_vr.transform.localRotation);
+            _player.Immobilize(false);
+        }
+    }
+    
+}
---
+++ HMDToggle.cs
@@ -0,0 +1,72 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class HMDToggle : UdonSharpBehaviour
+{
+    [UdonSynced]
+    private bool syncedValue;
+    private bool deserializing;
+    public HMDCheck vrSpawn;
+    private VRCPlayerApi localPlayer;
+
+    public Color onColor = Color.green;
+    public Color offColor = Color.red;
+    public MeshRenderer thisMesh;
+    public string[] _adminUsers;
+
+    private void Start()
+    {
+        localPlayer = Networking.LocalPlayer;
+        deserializing = false;
+        syncedValue = vrSpawn.bypass;
+        thisMesh = GetComponent<MeshRenderer>();
+        // init color for toggle
+        thisMesh.material.color = (vrSpawn.bypass ? onColor : offColor);
+        if (Networking.IsOwner(gameObject))
+            RequestSerialization();
+    }
+
+    private bool CheckAllowListStatus()
+    {
+        bool allow = false;
+        foreach (string _adminPlayers in _adminUsers)
+        {
+            if (localPlayer.displayName == _adminPlayers)
+                allow = true;
+        }
+
+        return allow;
+    }
+
+    public override void Interact()
+    {
+        bool allowed = CheckAllowListStatus();
+        if (allowed)
+        {
+            vrSpawn.bypass = !vrSpawn.bypass;
+            thisMesh.material.color = (vrSpawn.bypass ? onColor : offColor);
+            ToggleUpdate();
+        }
+    }
+
+    public override void OnDeserialization()
+    {
+        deserializing = true;
+        vrSpawn.bypass = syncedValue;
+        thisMesh.material.color = (syncedValue ? onColor : offColor);
+        deserializing = false;
+    }
+
+    public void ToggleUpdate()
+    {
+        if (deserializing)
+            return;
+        if (!Networking.IsOwner(gameObject))
+            Networking.SetOwner(localPlayer, gameObject);
+
+        syncedValue = vrSpawn.bypass;
+        RequestSerialization();
+    }
+}
